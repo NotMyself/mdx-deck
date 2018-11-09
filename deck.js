30525dcac26d480e130f352c4c5648946490d69f
Bobby Johnson <bobby.johnson@auth0.com>
2018-11-09 15:33:45 -0800

    testing

---
+++ deck.js
@@ -0,0 +1,13 @@
+import title from './slides/title.mdx'
+import about from './slides/about.mdx'
+import code from './slides/code.mdx'
+import end from './slides/end.mdx'
+
+export { default as theme } from './theme'
+
+export default [
+  ...title,
+  ...about,
+  ...code,
+  ...end
+]
---
+++ theme.js
@@ -0,0 +1,6 @@
+import theme from 'mdx-deck/themes'
+
+export default {
+  ...theme,
+  font: 'Proxima Nova, sans-serif'
+}
